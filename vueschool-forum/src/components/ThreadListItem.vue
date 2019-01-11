169931311ba16b80bd488a553cab6ea290b587bf
Nicole Chinn <nychinn@gmail.com>
2019-01-11 19:57:26 +1300

    start creating components

---
+++ vueschool-forum/src/components/ThreadListItem.vue
@@ -0,0 +1,46 @@
+<template>
+	
+	<div class="thread">
+		<div>
+		  <p>
+		      <a href="#">{{ thread.title }}</a>
+		  </p>
+		  <p class="text-faded text-xsmall">By <a href="profile.html">{{ user.name }}</a>, {{ thread.publishedAt }}</p>
+		</div>
+
+		<div class="activity">
+		  <p class="replies-count">
+		      {{ repliesCount }} replies
+		  </p>
+
+		  <!-- <img class="avatar-medium" src="https://firebasestorage.googleapis.com/v0/b/forum-2a982.appspot.com/o/images%2Favatars%2Fraynathan?alt=media&amp;token=bd9a0f0e-60f2-4e60-b092-77d1ded50a7e" alt=""> -->
+
+		  <!-- <span>
+		      <a class="text-xsmall" href="#">Ray-Nathan James</a>
+		      <p class="text-faded text-xsmall">3 hours ago</p>
+		  </span> -->
+		</div>
+	</div>
+
+</template>
+
+<script>
+	import sourceData from '@/data.json'
+
+	export default {
+		props: {
+			thread: {
+				required: true,
+				type: Object
+			}
+		},
+		computed: {
+			repliesCount () {
+				return Object.keys(this.thread.posts).length - 1
+			},
+			user () {
+				return sourceData.users[this.thread.userId]
+			}
+		}
+	}
+</script>
